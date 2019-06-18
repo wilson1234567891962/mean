5ff4f805156630c8da291f8ba2971171bb299e48
Alejandro gonzalez <wilson1234567891962@hotmail.com>
2019-06-17 20:45:41 -0500

    Create test to check form html

--- src/app/components/form/form.component.spec.ts
+++ src/app/components/form/form.component.spec.ts
@@ -1,16 +1,48 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FormComponent } from './form.component';
+import { RepositoryService } from 'src/app/services/repository.service';
+import { of } from 'rxjs';
+import { NavigationService } from 'src/app/services/navigation.service';
+import { MatSnackBar } from '@angular/material';
+import { NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ReactiveFormsModule, FormArray } from '@angular/forms';
 
-describe('FormComponent', () => {
+class RepositoryServiceStub {
+  savePins() {
+    return of(true);
+  }
+}
+
+class NavigationServiceStub {
+  goToPins() { }
+}
+
+class MatSnackBarStub {
+  open() {
+    return {
+      afterDismissed: () => {
+        return of(true);
+      }
+    };
+  }
+}
+
+fdescribe('FormComponent', () => {
   let component: FormComponent;
   let fixture: ComponentFixture<FormComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FormComponent ]
-    })
-    .compileComponents();
+      declarations: [FormComponent],
+      providers: [
+        { provide: RepositoryService, useClass: RepositoryServiceStub },
+        { provide: NavigationService, useClass: NavigationServiceStub },
+        { provide: MatSnackBar, useClass: MatSnackBarStub }
+      ],
+      schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA],
+      imports: [ReactiveFormsModule]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -21,5 +53,24 @@ describe('FormComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('When component is initilizated', () => {
+    it('Should create the forms', () => {
+      expect(Object.keys(component.firstFormGroup.controls)).toEqual(['title', 'author', 'description']);
+      expect(Object.keys(component.secondFormGroup.controls)).toEqual(['firstAsset', 'assets']);
+    });
+  });
+
+  describe('When addAsset is executed', () => {
+    it('Should add new group', () => {
+      const assets = <FormArray>component.secondFormGroup.get('assets');
+
+      component.addAsset();
+      component.addAsset();
+
+      console.log(Object.keys(assets.controls));
+      expect(Object.keys(assets.controls)).toEqual(['0', '1']);
+    });
   });
 });
