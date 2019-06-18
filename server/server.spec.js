2e1d45376994ef40bbc31f48630c0361b0e85c1f
Sem - 6 - INGENIERIA INDUSTRIAL: Wilson Alejandro Gonzalez Gaitan <wilsongonzalez.ag@academia.umb.edu.co>
2019-06-17 19:22:20 -0500

    Add axios, implement call post and check the service pdf

--- server/server.spec.js
+++ server/server.spec.js
@@ -1,19 +1,19 @@
-const express = require('express');
-const logger = require('morgan');
-const http = require('http');
-const PinsRouter = require('./routes/pins');
-const Pins = require('./models/Pins');
-const request = require('request');
-const axios = require('axios');
+const express = require("express");
+const logger = require("morgan");
+const http = require("http");
+const PinsRouter = require("./routes/pins");
+const Pins = require("./models/Pins");
+const request = require("request");
+const axios = require("axios");
 const app = express();
-var requestPromise = require('request-promise-native');
+var requestPromise = require("request-promise-native");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
-app.use('/api', PinsRouter.router);
-app.set('port', 3000);
+app.use("/api", PinsRouter.router);
+app.set("port", 3000);
 
-describe('Testing Router', () => {
+describe("Testing Router", () => {
   let server;
 
   beforeAll(() => {
@@ -25,15 +25,15 @@ describe('Testing Router', () => {
     server.close();
   });
 
-  describe('GET', () => {
+  describe("GET", () => {
     // GET 200
-    it('200 and find pin', done => {
+    it("200 and find pin", done => {
       const data = [{ id: 1 }];
-      spyOn(Pins, 'find').and.callFake(callBack => {
+      spyOn(Pins, "find").and.callFake(callBack => {
         callBack(false, data);
       });
 
-      request.get('http://localhost:3000/api', (error, response, body) => {
+      request.get("http://localhost:3000/api", (error, response, body) => {
         expect(response.statusCode).toBe(200);
         expect(JSON.parse(response.body)).toEqual([{ id: 1 }]);
         done();
@@ -41,53 +41,82 @@ describe('Testing Router', () => {
     });
 
     // GET 500
-    it('500', done => {
+    it("500", done => {
       const data = [{ id: 1 }];
-      spyOn(Pins, 'find').and.callFake(callBack => {
+      spyOn(Pins, "find").and.callFake(callBack => {
         callBack(true, data);
       });
 
-      request.get('http://localhost:3000/api', (error, response, body) => {
+      request.get("http://localhost:3000/api", (error, response, body) => {
         expect(response.statusCode).toBe(500);
         done();
       });
     });
   });
 
-  describe('POST', () => {
-    it('200', done => {
+  describe("POST", () => {
+    it("200", done => {
       const post = [
         {
-          title: 'Platzi',
-          author: 'Platzi',
-          description: 'Platzi rules',
+          title: "Platzi",
+          author: "Platzi",
+          description: "Platzi rules",
           percentage: 0,
           tags: [],
           assets: [
             {
-              title: 'Platzi',
-              description: 'description',
+              title: "Platzi",
+              description: "description",
               readed: false,
-              url: 'http://platzi.com'
+              url: "http://platzi.com"
             }
           ]
         }
       ];
 
-      spyOn(Pins, 'create').and.callFake((pin, callBack) => {
+      spyOn(Pins, "create").and.callFake((pin, callBack) => {
         callBack(false, {});
       });
 
-      spyOn(requestPromise, 'get').and.returnValue(
-        Promise.resolve('<title>Platzi</title><meta name="description" content="Platzi rules">')
+      spyOn(requestPromise, "get").and.returnValue(
+        Promise.resolve(
+          '<title>Platzi</title><meta name="description" content="Platzi rules">'
+        )
       );
 
-      const assets = [{ url: 'http://platzi.com' }];
+      const assets = [{ url: "http://platzi.com" }];
 
-      axios.post('http://localhost:3000/api', { title: 'title', author: 'author', description: 'description', assets }).then(res => {
-        expect(res.status).toBe(200);
-        done();
+      axios
+        .post("http://localhost:3000/api", {
+          title: "title",
+          author: "author",
+          description: "description",
+          assets
+        })
+        .then(res => {
+          expect(res.status).toBe(200);
+          done();
+        });
+    });
+
+    it("200 PDF", done => {
+      spyOn(Pins, "create").and.callFake((pins, callBack) => {
+        callBack(false, {});
       });
+
+      const assets = [{ url: "http://platzi.pdf" }];
+
+      axios
+        .post("http://localhost:3000/api", {
+          title: "title",
+          author: "author",
+          description: "description",
+          assets
+        })
+        .then(res => {
+          expect(res.status).toBe(200);
+          done();
+        });
     });
   });
 });
